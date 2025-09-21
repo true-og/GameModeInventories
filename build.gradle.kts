bc0a89526e3b9db030265473e39f784042a16712
NotAlexNoyle <alexnoyle@icloud.com>
2025-09-21 08:45:20 -0400

    Update gradle to 8.14.3 and shadow to 8.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
     id("java-library")
     id("com.diffplug.spotless") version "7.0.4"
-    id("com.gradleup.shadow") version "8.3.6"
+    id("com.gradleup.shadow") version "8.3.8"
     eclipse
 }
 
